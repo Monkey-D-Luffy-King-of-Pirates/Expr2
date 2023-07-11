bbcdf672c5ad12385d5e9b44959210631dedcb24
Демон Чайный <volshebnuyoleg666@yandex.ru>
2023-07-11 17:35:01 +0400

    Добавьте файлы проекта.

---
+++ Expr2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expr2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            Console.Write("Please, enter any three - digit number: ");
+
+            while (!int.TryParse(Console.ReadLine(), out number) && !(number > 100 && number < 999))
+
+            {
+                Console.Write("No, you wrong! Please, enter any three - digit number: ");
+            }
+
+            int x = number % 10;
+            int y = number / 10 % 10;
+            int z = number / 100;
+
+            Console.WriteLine(x * 100 + y * 10 + z);
+
+            Console.ReadKey();
+        }
+    }
+}
